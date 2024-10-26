7bcf0dd26dda1800785d2ad1650afeea8e01e339
Defouh-Desmond <desmonddefouh56@gmail.com>
2024-10-26 14:52:51 +0100

    third commit

--- includes/login.php
+++ includes/login.php
@@ -1,53 +1,31 @@
 <?php
 $info = (object)[];
+$data = [];
 
-    $data = false;
-
-    //validate info
-    $data['email'] = $DATA_OBJ->email;
-    if (empty($DATA_OBJ->email))
-    {
-        $Error = "Please enter a valid email";
-    }
-
-    if (empty($DATA_OBJ->password))
-    {
-        $Error = "Please enter a valid password";
-    }
-   
-
-    if ($Error == "") 
-    {
- 
-        $query = "select * from user where email = :email limit 1";
-        $result = $DB->read($query,$data);
-
-
-        if (is_array($result)) 
-        {
-            $result = $result[0];
-            if ($result -> password == $DATA_OBJ->password)
-            {
-                $_SESSION['userid'] = $result -> userid;
-                $info->message = "You've sucessfully logged in";
-                $info->data_type = "info";
-                echo json_encode($info);
-
-            }else{
-                $info->message = "Wrong password";
-                $info->data_type = "Error";
-                echo json_encode($info);
-            }
-        }else 
-        {
-            $info->message = "Wrong email";
+if (isset($_POST['email']) && isset($_POST['password'])) {
+    $arr['email'] = $_POST['email'];
+    $sql = "select * from users where email = :email limit 1";
+    $result = $DB->read($sql, $arr);
+    if (is_array($result)) {
+        $result = $result[0];
+        if (password_verify($_POST['password'], $result->password)) {
+            $_SESSION['userid'] = $result->userid;
+            $info->message = "You are logged in.";
+            $info->data_type = "info";
+            echo json_encode($info);
+        } else {
+            $info->message = "Incorrect password.";
             $info->data_type = "error";
             echo json_encode($info);
         }
-    }else 
-    {
-        $info->message = $Error;
+    } else {
+        $info->message = "User not found.";
         $info->data_type = "error";
         echo json_encode($info);
     }
-
+} else {
+    $info->message = "Please enter email and password.";
+    $info->data_type = "error";
+    echo json_encode($info);
+}
+?>
